c0cb7c73d1e7fd238bfbdf4177dff88a4051cb7a
Szymon Iwaniuk <s.f.iwaniuk@gmail.com>
2025-08-11 13:29:38 +0200

    refactor: refactor pass_item service func

--- src/domain/services.py
+++ src/domain/services.py
@@ -1,12 +1,16 @@
-from docx import Document
-import os
 import datetime
+import os
+from typing import Optional
+
+from docx import Document
 
 
-def fill_docx_template(it_worker, borrower, date, id, item, quantity):
+def fill_docx_template(it_worker: str, borrower: str, date: Optional[str], id: str, item: str, quantity: str) -> str:
     # Temporary hardcoded
     doc = Document(r"C:\Users\admin\Desktop\pliki\docx_writer\src\templates\pass_item_template.docx")
 
+    if date is None:
+        date = datetime.datetime.today().strftime("%Y-%m-%d")
 
     replacements = {
         "{{it_worker}}": it_worker,
@@ -14,16 +18,14 @@ def fill_docx_template(it_worker, borrower, date, id, item, quantity):
         "{{date}}": date,
         "{{id}}": id,
         "{{item}}": item,
-        "{{quantity}}": quantity
+        "{{quantity}}": quantity,
     }
 
-    # Replace in paragraphs
     for paragraph in doc.paragraphs:
         for placeholder, value in replacements.items():
             if placeholder in paragraph.text:
                 paragraph.text = paragraph.text.replace(placeholder, value)
 
-    # Replace in tables
     for table in doc.tables:
         for row in table.rows:
             for cell in row.cells:
@@ -31,9 +33,8 @@ def fill_docx_template(it_worker, borrower, date, id, item, quantity):
                     if placeholder in cell.text:
                         cell.text = cell.text.replace(placeholder, value)
 
-     # Save path
     desktop = os.path.join(os.path.expanduser("~"), "Desktop")
-    filename = f"Laptop_Contract_{borrower.replace(' ', '_')}_{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.docx"
+    filename = f"Przekazanie_sprzetu_{borrower.replace(' ', '_')}_{date}.docx"
     save_path = os.path.join(desktop, filename)
 
     # Save document
@@ -42,13 +43,13 @@ def fill_docx_template(it_worker, borrower, date, id, item, quantity):
     return save_path
 
 
-if __name__ == '__main__':
-        
+if __name__ == "__main__":
+
     fill_docx_template(
         it_worker="Szymon Iwaniuk",
-        borrower="Jan Kowalski",
+        borrower="Mike Wazowski",
         date="2025-08-11",
-        id="REQ-2025-001",
-        item="Monitor",
-        quantity="2",
+        id="K123",
+        item="Dell Laptop",
+        quantity="1",
     )
